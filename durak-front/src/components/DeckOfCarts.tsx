5b73158bd8d86ac119cb99a44283de7e9bd82a27
par1tet <mankova486@gmail.com>
2024-08-22 14:00:30 +0300

    add deck of cart

---
+++ durak-front/src/components/DeckOfCarts.tsx
@@ -0,0 +1,25 @@
+import cl from './DeckOfCarts.module.css'
+import { Cart } from '../utils/abstractClasses/cart'
+
+type propsDeck = {
+    carts: Cart[]
+}
+
+export const DeckOfCarts = ({carts}: propsDeck) => {
+    carts.map(cart =>{
+        console.log(cart.level)
+        console.log(cart.suit)
+    })
+    return (<>
+        <div className={cl.deck}>
+            {carts.map(cart =>
+                <img
+                    src={`/src/assets/images/carts/${cart.level}${cart.suit}.png`}
+                    alt="100"
+                    key={`${cart.level}:${cart.suit}`}
+                    className={cl['deck-img']}
+                />
+            )}
+        </div>
+    </>)
+}
--- durak-front/src/pages/GameWithUSelf/components/Game.tsx
+++ durak-front/src/pages/GameWithUSelf/components/Game.tsx
@@ -1,5 +1,12 @@
+import { DeckOfCarts } from "../../../components/DeckOfCarts"
+import { useStore } from "../../../hooks/useStore"
+import { rootStore } from "../../../store/rootStore"
+import { toJS } from "mobx"
+
 export const Game = ({}) => {
+    const myRootStore: rootStore = useStore()
+
     return (<>
-        Game
+        <DeckOfCarts carts={toJS(myRootStore.gameWithYourself.players[0].carts)}></DeckOfCarts>
     </>)
 }
